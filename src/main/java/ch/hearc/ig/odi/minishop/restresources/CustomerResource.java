293a7ce7162a777a41f351e53dcb67c77d55f9ec
CytriQ <lorrislamprecht1331@yahoo.com>
2019-03-31 17:37:31 +0200

    Implémentation méthode get customer

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -4,7 +4,39 @@
 
 package ch.hearc.ig.odi.minishop.restresources;
 
+import ch.hearc.ig.odi.minishop.business.Customer;
+import ch.hearc.ig.odi.minishop.exception.CustomerException;
+import ch.hearc.ig.odi.minishop.exception.NotFoundException;
+import ch.hearc.ig.odi.minishop.exception.NullFormException;
+import ch.hearc.ig.odi.minishop.exception.OrderException;
+import ch.hearc.ig.odi.minishop.exception.ProductException;
+import ch.hearc.ig.odi.minishop.services.PersistenceService;
+import java.text.ParseException;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.glassfish.jersey.internal.inject.Custom;
+
+@Path("customer")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 public class CustomerResource {
+
+  @Inject
+  private PersistenceService persistenceService;
+
+  @GET
+  public List<Customer> getCustomer() {
+    return persistenceService.getAllCustomers();
+  }
 
 }
 
