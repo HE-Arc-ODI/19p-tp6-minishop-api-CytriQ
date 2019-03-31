a2632337259cba8fcb18159826b16c02d890793c
CytriQ <lorrislamprecht1331@yahoo.com>
2019-03-31 17:40:01 +0200

    Implémentation méthode delete customer

--- src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
+++ src/main/java/ch/hearc/ig/odi/minishop/restresources/CustomerResource.java
@@ -15,6 +15,7 @@ import java.text.ParseException;
 import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -63,6 +64,18 @@ public class CustomerResource {
     } catch (CustomerException e) {
       e.printStackTrace();
       throw new NullFormException("customer couldn't have been updated.");
+    }
+  }
+
+  @DELETE
+  @Path("{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public void deleteCustomer(@PathParam("id") Long id) {
+    try {
+      persistenceService.deleteCustomer(id);
+    } catch (CustomerException e) {
+      e.printStackTrace();
+      throw new NullFormException("customer not deleted.");
     }
   }
 }
